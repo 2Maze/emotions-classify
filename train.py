a464c9d265c4dee63a166d2198e6d4a0517b3532
danil <justprogrammertoo@gmail.com>
2024-03-19 00:06:27 +0300

    Experiments with model

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 import torch
+import torchmetrics
 import numpy as np
 import lightning as L
 import torch.nn.functional as F
@@ -18,30 +19,34 @@ class LitModule(L.LightningModule):
         super().__init__()
         self.model = Wav2Vec2Classifier(num_classes)
 
+        self.val_loss = []
+        self.acc = torchmetrics.classification.Accuracy(task='multiclass', num_classes=num_classes)
+        self.f1 = torchmetrics.classification.F1Score(task='multiclass', num_classes=num_classes)
+
     def forward(self, x):
         return self.model(x)
 
     def training_step(self, batch, batch_idx):
         outputs = self.forward(batch['array'])
         loss = F.cross_entropy(outputs, batch['emotion'])
-        self.log('train_loss', loss)
+        self.log('train/loss', loss, on_step=True, on_epoch=False)
         return loss
 
     def validation_step(self, batch, batch_idx):
         outputs = self.forward(batch['array'])
         loss = F.cross_entropy(outputs, batch['emotion'])
-        #self.acc(outputs, y)
-        #self.f1(outputs, y)
-        self.log('val_loss', loss)
+        self.val_loss.append(loss.item())
+        self.log('val/acc', self.acc(outputs, batch['emotion']), on_step=False, on_epoch=True)
+        self.log('val/f1', self.f1(outputs, batch['emotion']), on_step=False, on_epoch=True)
+        self.log('val/loss', loss, on_step=True, on_epoch=False)
 
     def on_validation_epoch_end(self):
-        pass
-        #self.log('val_acc', self.acc)
-        #self.log('val_f1', self.f1)
+        self.log('val/mean_loss', np.array(self.val_loss).mean())
+        self.val_loss = []
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=1e-4)
-        scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[1000], gamma=0.1)
+        scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[80], gamma=0.1)
         return [optimizer], [scheduler]
 
 
@@ -63,19 +68,19 @@ def main():
 
     train_idx, val_idx = train_test_split(np.arange(len(dataset)),
                                           test_size=0.15,
-                                          random_state=999,
+                                          random_state=42,
                                           shuffle=True,
                                           stratify=dataset.emotion_labels)
     train_dataset = Subset(dataset, train_idx)
     val_dataset = Subset(dataset, val_idx)
 
-    train_dataloader = DataLoader(train_dataset, batch_size=1, shuffle=True, collate_fn=collate_fn)
-    val_dataloader = DataLoader(val_dataset, batch_size=1, shuffle=False, collate_fn=collate_fn)
+    train_dataloader = DataLoader(train_dataset, batch_size=10, shuffle=True, collate_fn=collate_fn)
+    val_dataloader = DataLoader(val_dataset, batch_size=10, shuffle=False, collate_fn=collate_fn)
 
     checkpoint_callback = ModelCheckpoint(
         dirpath='checkpoints',
         filename="classifier_{epoch:02d}",
-        every_n_epochs=2,
+        every_n_epochs=10,
         save_top_k=-1,
     )
     lr_monitor = LearningRateMonitor(logging_interval='epoch')
@@ -83,7 +88,7 @@ def main():
     model = LitModule(len(dataset.emotions))
     trainer = L.Trainer(accelerator='gpu',
                         devices=1,
-                        max_epochs=50,
+                        max_epochs=100,
                         callbacks=[checkpoint_callback, lr_monitor])
     trainer.fit(model, train_dataloader, val_dataloader)
 
