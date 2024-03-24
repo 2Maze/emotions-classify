a0840ffa0efd962c7a2e127cc821e49c682466a3
danil <justprogrammertoo@gmail.com>
2024-03-24 10:12:21 +0300

    Update dataset class

--- utils/data.py
+++ utils/data.py
@@ -1,17 +1,21 @@
 import librosa
 import json
 import os
+import numpy as np
 
 from torch.utils.data import Dataset
+from typing import Optional
 
 
 EMOTIONS = ['гнев', 'нейтраль', 'отвращение', 'печаль', 'радость', 'страх', 'удивление']
 STATES = ['нейтральное', 'отрицательное', 'положительное']
+SAMPLING_RATE = 16_000
 
 
 class EmotionDataset(Dataset):
     def __init__(self,
-                 annotation: str):
+                 annotation: str,
+                 padding_sec: Optional[int] = None):
         assert os.path.exists(annotation), 'Annotation file does not exist!'
         with open(annotation) as file:
             self.annotation = json.load(file)
@@ -23,6 +27,11 @@ class EmotionDataset(Dataset):
 
         self.folder = os.path.split(annotation)[0]
         self.annotation_files = list(self.annotation.keys())
+        self.padding_sec = padding_sec
+        if padding_sec:
+            self.target_length_samples = padding_sec * SAMPLING_RATE
+        else:
+            self.target_length_samples = None
 
     def __len__(self):
         return len(self.annotation)
@@ -30,6 +39,16 @@ class EmotionDataset(Dataset):
     def __getitem__(self, idx):
         key = self.annotation_files[idx]
         speech_array, sampling_rate = librosa.load(os.path.join(self.folder, key), sr=16_000)
+        assert sampling_rate == SAMPLING_RATE, 'Sampling rate input audio, is not correct!'
+        if self.padding_sec is not None:
+            if len(speech_array) < self.target_length_samples:
+                speech_array = np.concatenate((speech_array,
+                                              np.zeros(int(self.target_length_samples - len(speech_array)))),
+                                              axis=0,
+                                              dtype=np.float32)
+            else:
+                speech_array = speech_array[:self.target_length_samples]
+
         return {'array': speech_array,
                 'emotion': self.emotions.index(self.annotation[key]['emotion']),
                 'state': self.states.index(self.annotation[key]['state'])}
