272dac465757e3f3e533c097542baa2ea60cb9d1
DaniinXorchenabo <rkbcu@mail.ru>
2024-04-03 12:21:38 +0300

    :building_construction: move all python files to `src` folder.

--- src/train.py
+++ src/train.py
@@ -4,8 +4,8 @@ import numpy as np
 import lightning as L
 import torch.nn.functional as F
 
-from utils.data import EmotionDataset
-from model import Wav2Vec2Classifier
+from src.utils.data import EmotionDataset
+from src.model import Wav2Vec2Classifier
 from torch.utils.data import DataLoader
 from torch.nn.utils.rnn import pad_sequence
 from sklearn.model_selection import train_test_split
