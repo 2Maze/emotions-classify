9f744db348d98eef12b0e024233494089eabee32
DaniinXorchenabo <rkbcu@mail.ru>
2024-04-07 10:29:24 +0300

    :sparkles: add a test notebook.

---
+++ docker/before_learn.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+file="$1"
+
+tensorboard --logdir ./logs/lightning_logs &
+#jupyter lab  --allow-root  --ip=0.0.0.0 --NotebookApp.token=''  &
+ python ./src/"$file"
+jupyter lab  --allow-root  --ip=0.0.0.0 --NotebookApp.token=''
