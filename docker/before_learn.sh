13e6f91890fd3a28230ce1fa44cf5ca7ea782a07
DaniinXorchenabo <rkbcu@mail.ru>
2024-04-22 16:57:23 +0300

    :sparkles: some run configs changes.

--- docker/before_learn.sh
+++ docker/before_learn.sh
@@ -3,6 +3,6 @@
 file="$1"
 
 tensorboard --logdir ./logs/lightning_logs &
-#jupyter lab  --allow-root  --ip=0.0.0.0 --NotebookApp.token=''  &
- python ./src/"$file"
+jupyter lab  --allow-root  --ip=0.0.0.0 --NotebookApp.token=''  &
+python ./src/"$file"
 jupyter lab  --allow-root  --ip=0.0.0.0 --NotebookApp.token=''
